82d85f7183e207bf4405d427a723a8a85406ddf4
Warren Moore <wm@warrenmoore.net>
2022-11-29 16:15:55 -0800

    Migrated license from WTFPL to Unlicense for release 1.3.2

--- EasingPlayground-Mac/AppDelegate.m
+++ EasingPlayground-Mac/AppDelegate.m
@@ -1,16 +1,3 @@
-//
-//  AppDelegate.m
-//  EasingPlayground-Mac
-//
-//  Copyright (c) 2013, Auerhaus Development, LLC
-//  Copyright (c) 2022, Warren Moore
-//
-//  This program is free software. It comes without any warranty, to
-//  the extent permitted by applicable law. You can redistribute it
-//  and/or modify it under the terms of the Do What The Fuck You Want
-//  To Public License, Version 2, as published by Sam Hocevar. See
-//  http://sam.zoy.org/wtfpl/COPYING for more details.
-//
 
 #import "AppDelegate.h"
 
--- EasingPlayground-iOS/main.m
+++ EasingPlayground-iOS/main.m
@@ -1,15 +1,3 @@
-//
-//  main.m
-//  EasingPlayground-iOS
-//
-//  Copyright (c) 2013, Auerhaus Development, LLC
-//
-//  This program is free software. It comes without any warranty, to
-//  the extent permitted by applicable law. You can redistribute it
-//  and/or modify it under the terms of the Do What The Fuck You Want
-//  To Public License, Version 2, as published by Sam Hocevar. See
-//  http://sam.zoy.org/wtfpl/COPYING for more details.
-//
 
 #import <UIKit/UIKit.h>
 
